45b29ec976fe924ff87394044ccc8a5cc651fe19
Gabriel Lopes Rodrigues <gabriellopes@outlook.com>
2024-05-18 12:33:48 -0300

    feat: add header and default layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/defaultTheme";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<DefaultLayout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
