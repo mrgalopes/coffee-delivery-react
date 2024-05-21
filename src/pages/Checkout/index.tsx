d257dca2e88a0bbc9657074519a07ce9131e70e6
Gabriel Lopes Rodrigues <gabriellopes@outlook.com>
2024-05-21 20:59:17 -0300

    feat: add markup and style of left section of checkout page

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,84 @@
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money,
+} from "@phosphor-icons/react";
+import {
+  Card,
+  CardHeader,
+  Cards,
+  CheckoutContainer,
+  ComplementInput,
+  Input,
+  Inputs,
+  PaymentSelectionButton,
+  PaymentSelectionSection,
+} from "./styles";
+
 export function Checkout() {
-  return <h1>Checkout</h1>;
+  return (
+    <CheckoutContainer>
+      <form>
+        <section>
+          <h2>Complete o seu pedido</h2>
+          <Cards>
+            <Card>
+              <CardHeader $iconColor="yellow-dark">
+                <MapPinLine size={22} />
+                <div>
+                  <h3>Endereço de entrega</h3>
+                  <p>Informe o endereço onde deseja receber o pedido</p>
+                </div>
+              </CardHeader>
+              <Inputs>
+                <Input type="text" placeholder="CEP" />
+                <Input type="text" placeholder="Rua" />
+                <Input type="text" placeholder="Número" />
+                <ComplementInput
+                  type="text"
+                  placeholder="Complemento (Opcional)"
+                />
+                <Input type="text" placeholder="Bairro" />
+                <Input type="text" placeholder="Cidade" />
+                <Input type="text" placeholder="UF" />
+              </Inputs>
+            </Card>
+            <Card>
+              <CardHeader $iconColor="purple">
+                <CurrencyDollar />
+                <div>
+                  <h3>Pagamento</h3>
+                  <p>
+                    O pagamento é feito na entrega. Escolha a forma que deseja
+                    pagar
+                  </p>
+                </div>
+              </CardHeader>
+              <PaymentSelectionSection>
+                <PaymentSelectionButton>
+                  <CreditCard />
+                  <span>Cartão de Crédito</span>
+                </PaymentSelectionButton>
+                <PaymentSelectionButton>
+                  <Bank />
+                  Cartão de Débito
+                </PaymentSelectionButton>
+                <PaymentSelectionButton>
+                  <Money />
+                  Dinheiro
+                </PaymentSelectionButton>
+              </PaymentSelectionSection>
+            </Card>
+          </Cards>
+        </section>
+
+        <section>
+          <h2>Cafés selecionados</h2>
+          <div></div>
+        </section>
+      </form>
+    </CheckoutContainer>
+  );
 }
---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,146 @@
+import styled from "styled-components";
+import { defaultTheme } from "../../styles/themes/defaultTheme";
+
+export const CheckoutContainer = styled.div`
+  padding: 2.5rem 10rem;
+  form {
+    display: grid;
+    grid-template-columns: 40rem 1fr;
+    gap: 2rem;
+  }
+`
+
+export const Cards = styled.div`
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+`
+
+export const Card = styled.div`
+  background-color: ${props => props.theme["base-card"]};
+  padding: 2.5rem;
+  border-radius: 6px;
+`
+
+interface CardHeaderProps {
+  $iconColor: keyof typeof defaultTheme;
+}
+
+export const CardHeader = styled.div<CardHeaderProps>`
+  display: flex;
+  gap: 0.5rem;
+
+  svg {
+    color: ${props => props.theme[props.$iconColor]};
+  }
+
+  div h3 {
+    font-size: 1rem;
+    line-height: 1.3;
+    font-weight: 500;
+  }
+
+  div p {
+    font-size: 0.875rem;
+    line-height: 1.3;
+    font-weight: 500;
+  }
+
+  margin-bottom: 2rem;
+`
+
+export const Inputs = styled.div`
+  display: grid;
+  grid-template-columns: 200px 1fr 60px;
+  gap: 1rem 0.75rem;
+
+  & > :nth-child(1) { // CEP
+    grid-column: 1 / 2;
+    grid-row: 1 / 2;
+  }
+
+  & > :nth-child(2) { // Rua
+    grid-column: 1 / 4;
+    grid-row: 2 / 3;
+  }
+
+  & > :nth-child(3) { // Número
+    grid-column: 1 / 2;
+    grid-row: 3 / 4;
+  }
+
+  & > :nth-child(4) { // Complemento
+    grid-column: 2 / 4;
+    grid-row: 3 / 4;
+  }
+
+  & > :nth-child(5) { // Bairro
+    grid-column: 1 / 2;
+    grid-row: 4 / 5;
+  }
+
+  & > :nth-child(6) { // Cidade
+    grid-column: 2 / 3;
+    grid-row: 4 / 5;
+  }
+  
+  & > :nth-child(7) { // UF
+    grid-column: 3 / 4;
+    grid-row: 4 / 5;
+  }
+`
+
+export const Input = styled.input`
+  padding: 0.75rem;
+  background-color: ${props => props.theme["base-input"]};
+  border: 1px solid ${props => props.theme["base-button"]};
+  border-radius: 4px;
+
+  &:focus {
+    border: 1px solid ${props => props.theme["yellow-dark"]};
+    outline: none;
+  }
+`
+
+export const ComplementInput = styled(Input)`
+  &::placeholder {
+    // TODO descobrir se dá para estilizar para colocar o Opcional do outro lado
+    // como no design
+  }
+`
+
+export const PaymentSelectionSection = styled.div`
+  display: flex;
+  gap: 0.75rem;
+`
+
+export const PaymentSelectionButton = styled.button`
+  padding: 1rem;
+  width: 100%;
+
+  background-color: ${props => props.theme["base-button"]};
+  border: none;
+  border-radius: 6px;
+
+  display: flex;
+  gap: 0.75rem;
+  align-items: center;
+
+  text-transform: uppercase;
+  font-size: 0.75rem;
+  color: ${props => props.theme["base-text"]};
+
+  cursor: pointer;
+
+  svg {
+    font-size: 1rem;
+    color: ${props => props.theme.purple};
+  }
+
+  &:hover {
+    background-color: ${props => props.theme["base-hover"]};
+  }
+
+  // TODO colocar estado selecionado
+`
