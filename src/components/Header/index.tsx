bafa399a2fce4d1daec47ade6e6d85ca29b4cefb
Gabriel Lopes Rodrigues <gabriellopes@outlook.com>
2024-05-23 21:09:22 -0300

    feat: add links in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,24 @@
 import { CartButton, HeaderContainer, LocationButton } from "./styles";
 import Logo from "../../assets/Logo.svg";
 import { MapPin, ShoppingCart } from "@phosphor-icons/react";
+import { Link } from "react-router-dom";
 
 export function Header() {
   return (
     <HeaderContainer>
-      <img src={Logo} alt="logo do coffee delivery" />
+      <Link to="/">
+        <img src={Logo} alt="logo do coffee delivery" />
+      </Link>
       <div>
         <LocationButton>
           <MapPin size={22} weight="fill" />
           Porto Alegre, RS
         </LocationButton>
-        <CartButton>
-          <ShoppingCart size={22} weight="fill" />
-        </CartButton>
+        <Link to="/checkout">
+          <CartButton>
+            <ShoppingCart size={22} weight="fill" />
+          </CartButton>
+        </Link>
       </div>
     </HeaderContainer>
   );
